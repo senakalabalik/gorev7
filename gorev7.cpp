6a9d7e82f2c1232437d77a349d0a168cbbae5231
sena kalabalık <46752539+senakalabalik@users.noreply.github.com>
2022-12-25 19:29:30 +0300

    Update gorev7.cpp

--- gorev7.cpp
+++ gorev7.cpp
@@ -9,24 +9,19 @@ int cols= (argc-3)/rows;
 std::vector<int>invector;
 std::vector< std::vector<int> > matrix ;
 int newline=0;
-	for (int i = 0; i < rows; i++) {
-		invector.clear();
-		
-		for (int j = 0; j < cols; j++) {
-			
-			if (newline==0 ){
-				invector.push_back(atoi(argv[1])*atoi(argv[3+j+newline]));
-			}
-		
-			else {
-				invector.push_back(atoi(argv[3+j+newline]));
-			}
-			
+for (int i = 0; i < rows; i++) {
+	invector.clear();		
+	for (int j = 0; j < cols; j++) {	
+		if (newline==0 ){
+			invector.push_back(atoi(argv[1])*atoi(argv[3+j+newline]));
 		}
-		newline=newline+cols;
-
-		matrix.push_back(invector);
+		else {
+			invector.push_back(atoi(argv[3+j+newline]));
+		}			
 	}
+	newline=newline+cols;
+	matrix.push_back(invector);
+}
 	
 for (int i=0; i<matrix.size(); i++) {
 	for (int j=0; j<matrix[i].size(); j++) {
